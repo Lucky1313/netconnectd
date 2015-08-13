717a783a559824c15ce8f32ad3fe275c56446c14
David Hedin <lucky.hedin13@gmail.com>
2015-08-13 13:38:40 -0400

    Add Arch setup and service file

--- setup.py
+++ setup.py
@@ -30,12 +30,17 @@ EXTRAS_FOLDERS = [
 ]
 
 EXTRAS_FILES = [
-    #('/etc/init.d/', [('extras/netconnectd.init', 'netconnectd', 0755)]),
-    #('/etc/default/', [('extras/netconnectd.default', 'netconnectd', 0644)]),
+    ('/etc/init.d/', [('extras/netconnectd.init', 'netconnectd', 0755)]),
+    ('/etc/default/', [('extras/netconnectd.default', 'netconnectd', 0644)]),
     ('/etc/', [('extras/netconnectd.yaml', 'netconnectd.yaml', 0600)]),
     ('/etc/logrotate.d/', [('extras/netconnectd.logrotate', 'logrotate', 0644)]),
 ]
 
+ARCH_FILES = [
+    ('/usr/lib/systemd/system/', [('extras/netconnectd.service', 'netconnectd.service', 0755)]),
+    ('/etc/', [('extras/netconnectd.yaml', 'netconnectd.yaml', 0600)]),
+    ('/etc/logrotate.d/', [('extras/netconnectd.logrotate', 'logrotate', 0644)]),
+]
 
 def get_extra_tuple(entry):
     import os
@@ -159,11 +164,111 @@ class UninstallExtrasCommand(Command):
                 print("Error while removing %s (%s), please remove manually" % (folder, e.message))
 
 
+class InstallArchCommand(Command):
+    description = "install extras for Arch like service and config files"
+    user_options = [("force", "F", "force overwriting files if they already exist")]
+
+    def initialize_options(self):
+        self.force = None
+
+    def finalize_options(self):
+        if self.force is None:
+            self.force = False
+
+    def run(self):
+        global ARCH_FILES, EXTRAS_FOLDERS
+        import shutil
+        import os
+
+        for folder, mode in EXTRAS_FOLDERS:
+            try:
+                if os.path.exists(folder):
+                    os.chmod(folder, mode)
+                else:
+                    os.mkdir(folder, mode)
+            except Exception as e:
+                import sys
+
+                print("Error while creating %s (%s), aborting" % (folder, e.message))
+                sys.exit(-1)
+
+        for target, files in ARCH_FILES:
+            for entry in files:
+                extra_tuple = get_extra_tuple(entry)
+                if extra_tuple is None:
+                    print("Can't parse entry for target %s, skipping it: %r" % (target, entry))
+                    continue
+
+                path, filename, mode = extra_tuple
+                target_path = os.path.join(target, filename)
+
+                path_exists = os.path.exists(target_path)
+                if path_exists and not self.force:
+                    print("Skipping copying %s to %s as it already exists, use --force to overwrite" % (path, target_path))
+                    continue
+
+                try:
+                    shutil.copy(path, target_path)
+                    if mode:
+                        os.chmod(target_path, mode)
+                        print("Copied %s to %s and changed mode to %o" % (path, target_path, mode))
+                    else:
+                        print("Copied %s to %s" % (path, target_path))
+                except Exception as e:
+                    if not path_exists and os.path.exists(target_path):
+                        # we'll try to clean up again
+                        try:
+                            os.remove(target_path)
+                        except:
+                            pass
+
+                    import sys
+                    print("Error while copying %s to %s (%s), aborting" % (path, target_path, e.message))
+                    sys.exit(-1)
+
+
+class UninstallArchCommand(Command):
+    description = "uninstall extras for Arch like service and config files"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        global ARCH_FILES, EXTRAS_FOLDERS
+        import os
+
+        for target, files in ARCH_FILES:
+            for entry in files:
+                extra_tuple = get_extra_tuple(entry)
+                if extra_tuple is None:
+                    print("Can't parse entry for target %s, skipping it: %r" % (target, entry))
+
+                path, filename, mode = extra_tuple
+                target_path = os.path.join(target, filename)
+                try:
+                    os.remove(target_path)
+                    print("Removed %s" % target_path)
+                except Exception as e:
+                    print("Error while deleting %s from %s (%s), please remove manually" % (filename, target, e.message))
+
+        for folder, mode in EXTRAS_FOLDERS[::-1]:
+            try:
+                os.rmdir(folder)
+            except Exception as e:
+                print("Error while removing %s (%s), please remove manually" % (folder, e.message))
+
+
 def get_cmdclass():
     cmdclass = versioneer.get_cmdclass()
     cmdclass.update({
         'install_extras': InstallExtrasCommand,
-        'uninstall_extras': UninstallExtrasCommand
+        'uninstall_extras': UninstallExtrasCommand,
+        'install_arch': InstallArchCommand,
+        'uninstall_arch': UninstallArchCommand
     })
     return cmdclass
 
